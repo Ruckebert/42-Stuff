7b19f78e5daa30a377de2ce457ede7223203da15
Albert Aiyakup Ruckenbauer <aruckenb@c4r6p7.42vienna.com>
2025-02-26 16:23:54 +0100

    Updating just incase

---
+++ CPPS/CPP0/ex02/Account.cpp
@@ -0,0 +1,66 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Account.cpp                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aruckenb <aruckenb@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/02/26 13:48:34 by aruckenb          #+#    #+#             */
+/*   Updated: 2025/02/26 14:32:09 by aruckenb         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Account.hpp"
+#include <iostream>
+
+static int	getNbAccounts( void )
+{
+
+	
+}
+
+static int	getTotalAmount( void )
+{
+	
+}
+static int	getNbDeposits( void )
+{
+	
+}
+
+static int	getNbWithdrawals( void )
+{
+	
+}
+
+static void	displayAccountsInfos( void )
+{
+	std::cout << "index: " << ";" ;
+	std::cout << "index: " << ";" ;
+	std::cout << "index: " << ";" ;
+	std::cout << "index: " << ";" ;
+	std::cout << "index: " << ";" ;
+	std::cout << "index: " << ";" ;
+	std::cout << "index: " << ";" ;
+	std::cout << "index: " << ";" ;
+}
+
+void	makeDeposit( int deposit )
+{
+	
+}
+bool	makeWithdrawal( int withdrawal )
+{
+	
+	
+}
+int		checkAmount( void )
+{
+
+	
+}
+void	displayStatus( void )
+{
+
+	
+}
